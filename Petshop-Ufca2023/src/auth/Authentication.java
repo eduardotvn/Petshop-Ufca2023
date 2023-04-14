bc2152e46e3ab5646ad98e6b5fee3d81fbb1e01f
eduardotvn <eduardo_tvn@hotmail.com>
2023-04-14 18:05:57 -0300

    Atualizado

--- Petshop-Ufca2023/src/auth/Authentication.java
+++ Petshop-Ufca2023/src/auth/Authentication.java
@@ -18,28 +18,25 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 
 public class Authentication {
-	public static boolean authenticatePassword(String password, byte[] storedEncryptedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-	    byte[] salt = Arrays.copyOfRange(storedEncryptedPassword, 0, 16);
-	    byte[] iv = Arrays.copyOfRange(storedEncryptedPassword, 16, 32);
-	    byte[] encryptedPassword = Arrays.copyOfRange(storedEncryptedPassword, 32, storedEncryptedPassword.length);
+	public static boolean authenticatesenha(String senha, byte[] senhaSalva) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	    byte[] salt = Arrays.copyOfRange(senhaSalva, 0, 16);
+	    byte[] iv = Arrays.copyOfRange(senhaSalva, 16, 32);
+	    byte[] senhaCodificada = Arrays.copyOfRange(senhaSalva, 32, senhaSalva.length);
 	    
-	    // Generate a derived key using PBKDF2 with the same salt and parameters as during registration
 	    int iterationCount = 10000;
 	    int keyLength = 256;
 	    SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-	    PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, iterationCount, keyLength);
+	    PBEKeySpec keySpec = new PBEKeySpec(senha.toCharArray(), salt, iterationCount, keyLength);
 	    SecretKey secretKey = keyFactory.generateSecret(keySpec);
 	    byte[] keyBytes = secretKey.getEncoded();
 	    
-	    // Initialize the decryption cipher with the derived key and the stored IV
 	    SecretKeySpec keySpecs = new SecretKeySpec(keyBytes, "AES");
 	    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 	    cipher.init(Cipher.DECRYPT_MODE, keySpecs, new IvParameterSpec(iv));
 	    
-	    // Decrypt the stored encrypted password and compare it with the encrypted entered password
-	    byte[] decryptedPassword = cipher.doFinal(encryptedPassword);
-	    byte[] enteredPassword = password.getBytes();
-	    return Arrays.equals(decryptedPassword, enteredPassword);
+	    byte[] senhaDecodificada = cipher.doFinal(senhaCodificada);
+	    byte[] senhaFornecida = senha.getBytes();
+	    return Arrays.equals(senhaDecodificada, senhaFornecida);
 	}
 	
 	public static byte[] getChaveGerente()
